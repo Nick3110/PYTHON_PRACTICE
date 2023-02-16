6b7a796e01f6aa05f503d293350f81e85be1c54e
Nick3110 <114098584+Nick3110@users.noreply.github.com>
2023-02-16 18:11:15 +0530

    Add files via upload

---
+++ Varibales_Datatypes_python.py
@@ -0,0 +1,24 @@
+# variable's: They are container which stores specific values in memory.
+#this value are known as datatype 
+# datatypes:
+#  a)boolean: contains--> True/False
+#  b)integer: interger values like (-1,-7,8,50. etc.etc.)
+#  c)float :4.18, -5.63 etc etc type of values
+#  d)complex number: eg.6+4j
+#  e)string: anything enclose in inverted comma's
+#  f)mapped datatype: contains--> Dictionary (k:v) pair
+#  g)sequenced datatype : contains --> List,Tuple 
+
+x='12' # string variable
+#  checking  data type
+print(type(x))# built-in Fun to verify datatype
+y=12 #integer var
+print(type(y))
+z=complex(6,2)
+print(z,type(z))
+list1=['nikhil',15,89]
+print(list1,type(list1))
+dict1={'name':'nikhil','age':20}
+print(type(dict1))
+tuple1=(1,'abc','jkl',45)
+print(type(tuple1))
