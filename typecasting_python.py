c64840cd5e8c80af1a5c3b180083d7bc8e1596b8
Nick3110 <114098584+Nick3110@users.noreply.github.com>
2023-02-16 17:28:43 +0530

    Add files via upload

---
+++ typecasting_python.py
@@ -0,0 +1,12 @@
+x='12'
+y='45'
+print(int(x)+int(y))#output will be 57 
+# above code is explicit typecasting. 
+# explicit typecasting means user/coder does it. 
+# implicit typecasting means python env. does it irrespective of user's choice
+# basically in Python order of datatypr is not same hence while performing any function it convert output according to higher order datatype 
+# eg. in float & Int, float have higher order hence while performing addition between them output will be float
+a=12
+b=9.8
+print(a+b)
+# output will be 21.8
